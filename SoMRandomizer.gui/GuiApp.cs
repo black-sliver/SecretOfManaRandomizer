1dee6f204fa342a3626b2cd64beb8220b839e341
black-sliver <59490463+black-sliver@users.noreply.github.com>
2026-01-17 01:40:27 +0100

    Change AssemblyName of core to SoMRandomizer.core
    This should resolve naming conflicts with the GUI exe.

--- SoMRandomizer.gui/GuiApp.cs
+++ SoMRandomizer.gui/GuiApp.cs
@@ -5,7 +5,7 @@ namespace SoMRandomizer.gui
 {
     internal static class GuiApp
     {
-        internal static void Run()
+        internal static void Run() // needs to be public so it doesn't get inlined
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
